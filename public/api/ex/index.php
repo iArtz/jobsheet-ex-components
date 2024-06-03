8b3e6711257346eedda2d26d3c685609b5ef9faf
iArt <iart@aboutdev.io>
2024-06-03 09:05:46 +0700

    Add Radial air gab calculation

--- public/api/ex/index.php
+++ public/api/ex/index.php
@@ -4,7 +4,24 @@ require_once dirname(__DIR__) . '../../../vendor/autoload.php';
 
 use Jobsheet\Ex\Utils\Helper;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    Helper::print(count($_POST));
-    Helper::export($_POST);
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        Helper::print(count($_POST));
+        Helper::export($_POST);
+        break;
+
+    default:
+        if (empty($_SERVER['PATH_INFO'])) return;
+        preg_match('/\d.*/', $_SERVER['PATH_INFO'], $id);
+        $data = [
+            'id' => array_pop($id),
+            'data' => [
+                'D' => '',
+                'component' => 'radial_air_gab_calculation',
+            ]
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        break;
 }
--- src/Type/A/Containers/A.php
+++ src/Type/A/Containers/A.php
@@ -27,6 +27,7 @@ use Jobsheet\Ex\Type\A\Components\MechanicalRepairing;
 use Jobsheet\Ex\Type\A\Components\Pictures;
 use Jobsheet\Ex\Type\A\Components\PicturesDC;
 use Jobsheet\Ex\Type\A\Components\PolarizationIndex;
+use Jobsheet\Ex\Type\A\Components\RadialAirGabCalculation;
 use Jobsheet\Ex\Type\A\Components\ResistanceInductanceTest;
 use Jobsheet\Ex\Type\A\Components\ResistanceInductanceTestDC;
 use Jobsheet\Ex\Type\A\Components\ResistanceInductanceTestSinglePhase;
@@ -94,6 +95,7 @@ class A extends Container
         MechanicalRepairing::class,
         SummaryOfElectricalWork::class,
         SummaryOfMechanicalWork::class,
+        RadialAirGabCalculation::class,
     ];
 
     protected static function renderHTML(): string
