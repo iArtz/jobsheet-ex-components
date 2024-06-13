4a65a1a45e3aa44c2b6ca04166deb4c5d2825340
iArt <iart@aboutdev.io>
2024-06-13 17:35:26 +0700

    Update Fixtures

--- public/index.php
+++ public/index.php
@@ -4,13 +4,20 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Jobsheet\Ex\Classes\Abstracts\MotorType;
 use Jobsheet\Ex\Type\A\Containers\A;
+use Jobsheet\Ex\Utils\Helper;
 
 set_error_handler('Jobsheet\Ex\Core\Error::errorHandler');
 set_exception_handler('Jobsheet\Ex\Core\Error::exceptionHandler');
 
-// $json = file_get_contents('http://localhost/api/ex/1');
+$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/api/ex/' . Helper::randomString();
+$context = stream_context_create([
+    'ssl' => [
+        'verify_peer' => false,
+    ]
+]);
 
-// $data = json_decode($json, true);
+$json = file_get_contents($url, false, $context);
+$data = json_decode($json, true);
 
 $data['showAll'] = true;
 
